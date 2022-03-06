b2ec2f35c50439405dc4d785c271de8a32347445
angela cuadros <angelascc16@gmail.com>
2022-03-06 16:31:51 -0500

    function exercise

--- Fundamentals-2/script.js
+++ Fundamentals-2/script.js
@@ -1,9 +1,12 @@
 'use strict';
 
 function fruitProcessor(apples, oranges) {
-  const juice = `Juice with ${apples}apples and ${oranges} oranges.`;
+  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
   return juice;
 }
 
 const appleJuice = fruitProcessor(5, 0);
 console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log (appleOrangeJuice);
