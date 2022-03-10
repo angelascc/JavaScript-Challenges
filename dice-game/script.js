8ee3df5ea3967f60f1ea745c097480a94683f559
angela cuadros <angelascc16@gmail.com>
2022-03-10 12:50:48 -0500

    hidden dice

---
+++ dice-game/script.js
@@ -0,0 +1,7 @@
+const score0El = document.getElementById('score--0');
+const score1El = document.getElementById('score--1');
+const hiddenDice = document.querySelector('.dice');
+
+score0El.innerText = 0;
+score1El.innerText = 0;
+hiddenDice.classList.add('hidden');
