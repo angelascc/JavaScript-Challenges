a5dc175695a1df8124a53019200fa3d8520448b6
angela cuadros <angelascc16@gmail.com>
2022-03-10 13:37:29 -0500

    rolling the dice

--- dice-game/script.js
+++ dice-game/script.js
@@ -1,7 +1,26 @@
 const score0El = document.getElementById('score--0');
 const score1El = document.getElementById('score--1');
-const hiddenDice = document.querySelector('.dice');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+const diceEl = document.querySelector('.dice');
+const btnNewGame = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
 
 score0El.innerText = 0;
 score1El.innerText = 0;
-hiddenDice.classList.add('hidden');
+diceEl.classList.add('hidden');
+let currentScore = 0;
+
+btnRoll.addEventListener('click', () => {
+  diceEl.classList.remove('hidden');
+
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  console.log(dice);
+  diceEl.src = `dice-${dice}.png`;
+
+  if (dice !== 1) {
+    currentScore += dice;
+    current0El.innerText = currentScore;
+  }
+})
